a1ad4b9286453efb8b4a15bf1d191f62c75647a3
Joel Lord <joelphy@gmail.com>
2018-02-14 18:40:31 -0500

    Added Google analytics and sponsor logo

--- src/features/common/TitleBar.js
+++ src/features/common/TitleBar.js
@@ -8,7 +8,12 @@ export default class TitleBar extends Component {
   render() {
     return (
       <div className="common-title-bar">
-        <img src={require('../../images/logo.png')} />
+        <span className="left">
+          <img src={require('../../images/logo.png')} />
+        </span>
+        <span className="right crafted-by">
+          Crafted by <img src={require("../../images/auth0logo.png")} />
+        </span>
       </div>
     );
   }

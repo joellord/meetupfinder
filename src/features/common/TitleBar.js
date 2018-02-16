ae4e70330c097d1aec9cfd602a27409ea7bcde4b
Joel Lord <joelphy@gmail.com>
2018-02-15 20:51:09 -0500

    Use of Algolia and added link to sponsor website

--- src/features/common/TitleBar.js
+++ src/features/common/TitleBar.js
@@ -12,7 +12,9 @@ export default class TitleBar extends Component {
           <img src={require('../../images/logo.png')} />
         </span>
         <span className="right crafted-by">
-          Crafted by <img src={require("../../images/auth0logo.png")} />
+          <a href="http://auth0.com/?utm_source=meetupfinder&utm_medium=es&utm_campaign=landing_page" target="_blank">
+            Crafted by <img src={require("../../images/auth0logo.png")} />
+          </a>
         </span>
       </div>
     );
